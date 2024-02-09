97147065b2fb37daba593668147a749f95de7d1c
pranay <pranayharishchandra@gmail.com>
2024-02-10 00:17:57 +0530

    snippet page

--- src/app/snippet/[id]/page.tsx
+++ src/app/snippet/[id]/page.tsx
@@ -9,7 +9,7 @@ interface SnippetPageProp {
 
 async function SnippetPage(props: SnippetPageProp) {
 
-  await new Promise((r) => setTimeout(r,200))
+  await new Promise((r) => setTimeout(r, 200))
   const { id } = props.params;
 
   try {
@@ -26,16 +26,32 @@ async function SnippetPage(props: SnippetPageProp) {
         </div>
       );
     }
-    
+
 
     return (
       <div key={snippet.id} className="border border-blue-500 m-2 rounded-md shadow-md">
-      <div className="flex-row m-2">
-        
-        <h1 className="text-2xl text-gray-700 p-1 border-b border-red-300">{snippet.title}</h1>
-        <h1 className="text-md text-gray-700 p-1">{snippet.snippet}</h1>
+        <div className="flex-row m-2">
+          <div className="flex justify-between border-b border-red-300 pb-2">
+
+            <div>
+              <h1 className="text-2xl text-gray-700 p-1 ">{snippet.title}</h1>
+            </div>
+
+            <div className="flex">
+              <button className="mx-1 px-2 bg-blue-300 hover:bg-blue-400 rounded-lg">Edit</button>
+              <button className="mx-1 px-2 bg-blue-300 hover:bg-blue-400 rounded-lg">Delete</button>
+            </div>
+
+          </div>
+
+          <pre>
+            <p className="rounded-md m-2 text-md  p-2 bg-violet-200 text-black">
+              {snippet.snippet}
+            </p>
+          </pre>
+
+        </div>
       </div>
-    </div>
     );
   } catch (error) {
     console.error("Error fetching snippet:", error);
