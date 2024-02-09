364fd90995f23317f5a3f969379343ed1301b813
pranay <pranayharishchandra@gmail.com>
2024-02-09 19:47:57 +0530

    retriving and showing snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
+export default async function Home() {
+
+  const snippets = await db.snippet.findMany()
+
+  const codeSnippets = snippets.map (snippet => (
+    <div key={snippet.id} className="border border-blue-500 m-2 rounded-md shadow-md">
+      <div className="flex-row m-2">
+      <h1 className="text-2xl text-gray-700 p-1 border-b border-red-300">{snippet.title}</h1>
+      <h1 className="text-md text-gray-700 p-1">{snippet.snippet}</h1>
+      </div>
+    </div>
+  ))
+
+
+
   return (
-    <div>
-      Hello Home page
+    <div >
+
+    <div className="flex-row w-[70vh] mx-auto">
+      {codeSnippets}
+    </div>
     </div>
   );
 }
