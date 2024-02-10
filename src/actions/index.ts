9223a46303d9642e8e0309140416c4458f2ab3d4
pranay <pranayharishchandra@gmail.com>
2024-02-10 20:26:18 +0530

    validation when creation

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,5 +19,35 @@ export async function deleteSnippet(id: number) {
   redirect(`/`)
 }
 
+  // create
+export  async function createSnippet(formState: {message: string}, formData: FormData) {
+
+
+    // Check the user input, if valid then add to db
+    const title = formData.get('title') as string
+    const code  = formData.get('code') as string
+
+
+    if (typeof(title) !== 'string' || title.length < 2 )
+      return { message: "title must be longer" }
+
+    if (typeof(code) !== 'string' || code.length < 3 )
+      return { message: "code must be longer" }
+
+    // add it to db
+    const snippet = await db.snippet.create({
+      data: {
+        title: title,
+        snippet: code
+      }
+    })
+
+    console.log(snippet)
+
+    // redirect back to root page
+    redirect('/')
+    
+  }
+
 
 
--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -1,33 +1,16 @@
-// pages/CreateSnippetPage.js
+'use client'
+
 import React from 'react';
-import { db } from '@/db';
-import { redirect } from 'next/navigation';
+// import { db } from '@/db';
+// import { redirect } from 'next/navigation';
+import * as actions from '@/actions'
+import { useFormState } from 'react-dom';
 
 const CreateSnippetPage = () => {
 
-  // create
-  async function createSnippet(formData: FormData) {
-    // This needs to be a server
-    'use server'
-
-    // Check the user input, if valid then add to db
-    const title = formData.get('title') as string
-    const code  = formData.get('code') as string
-
-    // add it to db
-    const snippet = await db.snippet.create({
-      data: {
-        title: title,
-        snippet: code
-      }
-    })
-
-    console.log(snippet)
-
-    // redirect back to root page
-    redirect('/')
-    
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: '',
+  })
 
 
 
@@ -36,7 +19,8 @@ const CreateSnippetPage = () => {
     <div className="flex  justify-center bg-gray-100 h-screen">
       <div className="bg-white p-8 rounded-md shadow-md max-w-[70vw] min-w-[390px] sm:w-screen my-5">
         <h1 className="text-3xl font-semibold mb-4 text-center">Create Code Snippet</h1>
-        <form action={createSnippet}>
+        {/* <form action={createSnippet}> */}
+        <form action={action}>
           <div className="mb-4">
             <label htmlFor="title" className="block text-gray-700 font-medium">Title:</label>
             <input type="text" id="title" name="title" className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
@@ -45,11 +29,16 @@ const CreateSnippetPage = () => {
             <label htmlFor="snippet" className="block text-gray-700 font-medium">Snippet:</label>
             <textarea id="snippet" name="code" rows={15} className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"></textarea>
           </div>
+
+          {formState.message && <div className='bg-red-400 text-white rounded-md  w-full mb-1 mt-[-1.5rem] p-1 border border-red-600'>
+            <h1 className="text-lg  text-center text-white-600">{formState.message}</h1>
+          </div>}
+
           <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 w-full">Submit</button>
         </form>
       </div>
     </div>
   );
-}; 
+};
 
 export default CreateSnippetPage;
